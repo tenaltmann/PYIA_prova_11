e08ddd2e7c5003a29f15ace2d54bdfe6c5176d49
Ricardo Altmann <tenaltmann@gmail.com>
2025-07-21 19:09:47 -0300

    Criação da classe animal e metodo de emitir som genérico

---
+++ prova_11.py
@@ -0,0 +1,8 @@
+class Animal:
+    def __init__(self, especie):
+        self.especie = especie
+
+    def emitir_som_genérico(self):
+        print("Este animal faz um som genérico")
+        return
+    
